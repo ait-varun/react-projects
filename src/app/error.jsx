a8f73fa22764a58864c3a380a1a62e30a263a908
Varun <aitvarunbhatia@gmail.com>
2024-08-01 17:22:48 +0530

    updated error pages

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -4,8 +4,8 @@ import { Link } from "next-view-transitions";
 export default function Error() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
-        <div className="text-center">
+      <div className="flex items-center justify-center h-screen fixed top-0 left-0 w-full bg-gray-700">
+        <div className="text-center text-white">
           <h1 className="text-6xl font-bold">500</h1>
           <p className="text-2xl font-semibold">Internal server error</p>
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-lg transition duration-300 ease-in-out">
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,8 +3,8 @@ import { Link } from "next-view-transitions";
 export default function NotFound() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen fixed top-0 left-0 w-full bg-gray-900">
-        <div className="text-center">
+      <div className="flex items-center justify-center h-screen fixed top-0 left-0 w-full bg-gray-700">
+        <div className="text-center text-white">
           <h1 className="text-6xl font-bold">404</h1>
           <p className="text-2xl font-semibold">Page not found</p>
           <Link href="/" className="text-blue-500 hover:underline">
