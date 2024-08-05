9426e58e3af53784d59f152eec3cdb0f3117ff7d
Varun <aitvarunbhatia@gmail.com>
2024-08-05 12:34:14 +0530

    added comment box component

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,11 @@
 import Search from "../../components/common/searchBox";
+import CommentBox from "../../components/common/commentBox";
 
 export default function Contact() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4 gap-10">
       <Search />
+      <CommentBox />
     </div>
   );
 }
